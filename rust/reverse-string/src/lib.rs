ce5cf50323892bdb80ea0ade1dda2150ea331899
Kevin Jing Qiu <kevin@idempotent.ca>
2020-01-18 14:51:16 -0500

    Simplify

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut output = Vec::new();
-    for (_, ch) in input.chars().rev().enumerate() {
-        output.push(ch);
-    }
-    output.into_iter().collect()
+    input.chars().rev().collect()
 }
