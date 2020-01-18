a8c1aac73f3872cc3dc7b38f2a59ca9f5360d772
Kevin Jing Qiu <kevin@idempotent.ca>
2020-01-18 13:29:22 -0500

    reverse_string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,11 +1,7 @@
 pub fn reverse(input: &str) -> String {
-    if input.len() == 0 {
-        return String::from("");
-    }
-    let input_bytes = input.as_bytes();
     let mut output = Vec::new();
-    for i in (0..input.len()).rev() {
-        output.push(input_bytes[i])
+    for (_, ch) in input.chars().rev().enumerate() {
+        output.push(ch);
     }
-    String::from_utf8(output).unwrap()
+    output.into_iter().collect()
 }
