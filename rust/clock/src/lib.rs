727cb7b567e0949a321c17afd28f9a4cebdc647d
Kevin Jing Qiu <kevin@idempotent.ca>
2020-01-21 21:32:31 -0500

    Most rudimentary case

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,23 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock{hours: hours, minutes: minutes}
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
